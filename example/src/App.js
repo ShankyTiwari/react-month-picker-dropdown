ece678737c4d5dc565cfe45b14c01bb557d93616
Shashank <shanshank69@gmail.com>
2020-05-28 19:02:35 +0530

    init create-react-library@3.0.9

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-month-picker-dropdown'
+import 'react-month-picker-dropdown/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
